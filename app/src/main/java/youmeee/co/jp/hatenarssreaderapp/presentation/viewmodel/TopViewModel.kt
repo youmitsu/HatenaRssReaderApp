febf5d6712da35c010ca0666747696203f2b3d2c
YuMitsuhori <ymbullseye@gmail.com>
2019-02-12 23:46:47 +0900

    add dependencies aac

---
+++ app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/viewmodel/TopViewModel.kt
@@ -0,0 +1,18 @@
+package youmeee.co.jp.hatenarssreaderapp.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import youmeee.co.jp.hatenarssreaderapp.net.entity.HatebuFeed
+import youmeee.co.jp.hatenarssreaderapp.util.ApiResult
+
+class TopViewModel : ViewModel() {
+
+    val result: LiveData<ApiResult<HatebuFeed>>
+        get() {
+            return mResult
+        }
+    val mResult = MutableLiveData<ApiResult<HatebuFeed>>()
+
+    
+}
