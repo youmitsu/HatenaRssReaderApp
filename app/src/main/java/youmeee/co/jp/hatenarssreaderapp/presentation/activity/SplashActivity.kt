2e640bd436d734bd7c029414feb41ad2a2f18c2e
yu mitsuhori <ymbullseye@gmail.com>
2019-06-24 20:41:59 +0900

    icon調整

--- app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/SplashActivity.kt
+++ app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/SplashActivity.kt
@@ -3,17 +3,13 @@ package youmeee.co.jp.hatenarssreaderapp.presentation.activity
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import android.content.SharedPreferences
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.preference.PreferenceManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import youmeee.co.jp.hatenarssreaderapp.BuildConfig
@@ -44,6 +40,7 @@ class SplashActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.message).text = message
             Glide.with(this)
                     .load(url)
+                    .apply(RequestOptions.circleCropTransform())
                     .into(findViewById(R.id.splash_image))
             moveToMainActivity()
         }
@@ -54,7 +51,7 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
-        }, 500)
+        }, 750)
     }
 
     private fun initRemoteConfigEventSettings() {
