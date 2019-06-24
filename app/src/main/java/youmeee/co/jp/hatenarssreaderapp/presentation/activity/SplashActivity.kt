7e960ba66c90be8d69b122f5b61102b00f4c74ae
yu mitsuhori <ymbullseye@gmail.com>
2019-06-24 12:07:34 +0900

    スプラッシュで画像表示

--- app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/SplashActivity.kt
+++ app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/SplashActivity.kt
@@ -3,11 +3,17 @@ package youmeee.co.jp.hatenarssreaderapp.presentation.activity
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.preference.PreferenceManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import youmeee.co.jp.hatenarssreaderapp.BuildConfig
@@ -33,9 +39,12 @@ class SplashActivity : AppCompatActivity() {
 
         initRemoteConfigEventSettings()
 
-        fetchWelcomeMessage { title, message ->
+        fetchWelcomeMessage { title, message, url ->
             findViewById<TextView>(R.id.title).text = title
             findViewById<TextView>(R.id.message).text = message
+            Glide.with(this)
+                    .load(url)
+                    .into(findViewById(R.id.splash_image))
             moveToMainActivity()
         }
     }
@@ -64,10 +73,12 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
-    private fun fetchWelcomeMessage(isFetched: (title: String, message: String) -> Unit) {
+    private fun fetchWelcomeMessage(isFetched: (title: String, message: String, url: String) -> Unit) {
         remoteConfig.apply {
             fetchAndActivate().addOnCompleteListener {
-                isFetched(getString("welcome_title"), getString("welcome_message"))
+                isFetched(getString("welcome_title"),
+                        getString("welcome_message"),
+                        getString("welcome_image_url"))
                 sharedPreferences.edit().putBoolean("CONFIG_STALE", false).apply()
             }
         }
