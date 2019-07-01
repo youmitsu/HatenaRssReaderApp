2993ba41b4415a16edfbed4534bbe3f7121c5145
yu mitsuhori <ymbullseye@gmail.com>
2019-07-01 23:51:55 +0900

    toolbarの実装

--- app/src/main/java/youmeee/co/jp/hatenarssreaderapp/NewDetailFragment.kt
+++ app/src/main/java/youmeee/co/jp/hatenarssreaderapp/NewDetailFragment.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import youmeee.co.jp.hatenarssreaderapp.databinding.FragmentNewDetailBinding
 
 class NewDetailFragment : Fragment() {
 
     lateinit var binding: FragmentNewDetailBinding
-    val args: NewDetailFragmentArgs by navArgs()
+    private val args: NewDetailFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_detail, container, false)
-        binding.entry = args.entry
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.entry = args.entry
+        (activity as AppCompatActivity).apply {
+            setSupportActionBar(binding.toolbar)
+            supportActionBar?.title = null
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
