a77d165900ddabadc1f6a22a9d61156f6cde714e
yu mitsuhori <ymbullseye@gmail.com>
2019-07-08 01:07:46 +0900

    navigateupでバック

--- app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/youmeee/co/jp/hatenarssreaderapp/presentation/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package youmeee.co.jp.hatenarssreaderapp.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import youmeee.co.jp.hatenarssreaderapp.R
@@ -14,13 +15,21 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var androidInjector: DispatchingAndroidInjector<Fragment>
 
+    lateinit var hostFragment: NavHostFragment
+
     override fun supportFragmentInjector() = androidInjector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         showSplash()
         setContentView(R.layout.activity_main_new)
+        hostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         setSupportActionBar(findViewById(R.id.tool_bar))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        hostFragment.navController.popBackStack()
+        return super.onSupportNavigateUp()
     }
 
     private fun showSplash() {
